2876242866f29e5ebc5932ea35cdc8ed86dba485
Paul Adams <ghusiyy@gmail.com>
2023-11-09 14:44:54 -0800

    first commit

---
+++ src/main/java/com/example/cyberchatter/CyberchatterApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyberchatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberchatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberchatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cyberchatter/CyberchatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyberchatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberchatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

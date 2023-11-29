d639676e0171b2e22ad7d424ef9c303c4863bbe9
Paul Adams <ghusiyy@gmail.com>
2023-11-28 18:08:56 -0800

    restructure home page

--- server.go
+++ server.go
@@ -3,10 +3,15 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
+
+type PageVariables struct {
+	Title string
+}
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -36,10 +41,28 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// HomePage handles requests to the home page.
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	// Load HTML template
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Variables to be used in the template
+	var pageVariables PageVariables
+	pageVariables.Title = "CyberChatter"
+
+	// Execute the template
+	err = tmpl.Execute(w, pageVariables)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
+	http.HandleFunc("/", HomePage)
 	// Serve static files
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/ws", handleConnections)
